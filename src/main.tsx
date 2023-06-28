86ffcaec219d1139f43688152e9f795aa5ceb6b1
dimitrikarpov <karpov.dimon@gmail.com>
2023-06-28 10:40:18 +0500

    111

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Layout } from "./components/layout"
 import { FromCollection } from "./components/from-collection/from-collection"
 import { FromLocal } from "./components/from-local/from-local"
 
-console.log("import.meta.env.BASE_URL", import.meta.env.BASE_URL)
+console.log("import.meta.env.BASE_URL")
 
 const router = createBrowserRouter(
   [

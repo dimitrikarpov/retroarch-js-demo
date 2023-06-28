d6c12d261130baaf66b0e0fdb2794a10f3c1cf1c
dimitrikarpov <karpov.dimon@gmail.com>
2023-06-28 09:58:21 +0500

    update vite base path

--- vite.config.ghpages.ts
+++ vite.config.ghpages.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/retroarch-react-demo-site",
+  base: "/retroarch-js-demo",
   plugins: [react()],
 })

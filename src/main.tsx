b275d79334472db71983e61719a20775f4849234
dimitrikarpov <karpov.dimon@gmail.com>
2023-06-28 10:38:36 +0500

    revert config

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,8 @@ import { Layout } from "./components/layout"
 import { FromCollection } from "./components/from-collection/from-collection"
 import { FromLocal } from "./components/from-local/from-local"
 
+console.log("import.meta.env.BASE_URL", import.meta.env.BASE_URL)
+
 const router = createBrowserRouter(
   [
     {
--- vite.config.ghpages.ts
+++ vite.config.ghpages.ts
@@ -2,11 +2,7 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => {
-  return {
-    define: {
-      base: "/retroarch-js-demo",
-      plugins: [react()],
-    },
-  }
+export default defineConfig({
+  base: "/retroarch-js-demo",
+  plugins: [react()],
 })
